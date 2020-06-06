614d07ca0cd7e2080a42dcd65f612683c50ccb7b
mohmj <42730422+mohmj@users.noreply.github.com>
2020-06-06 23:28:02 +0300

    Update git ignore file. Connect the app with firebase. Try to changethe icon of app but had some problems.

--- app/src/main/java/com/ficruty/caocap/Controller/MainActivity.kt
+++ app/src/main/java/com/ficruty/caocap/Controller/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.GridLayout
 import androidx.recyclerview.widget.GridLayoutManager
-import com.ficruty.caocap.Adapter.ExploreAdapter
+//import com.ficruty.caocap.Adapter.ExploreAdapter
 import com.ficruty.caocap.Database.caocap
 import com.ficruty.caocap.Database.mySimpleData
 import com.ficruty.caocap.Database.mySimpleData2
@@ -13,15 +13,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var exploreAdapter: ExploreAdapter
+//    lateinit var exploreAdapter: ExploreAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val exploreAdapter = ExploreAdapter(this, mySimpleData2.cs)
-        explore.adapter = exploreAdapter
-        explore.layoutManager = GridLayoutManager(this,2)
+//        val exploreAdapter = ExploreAdapter(this, mySimpleData2.cs)
+//        explore.adapter = exploreAdapter
+//        explore.layoutManager = GridLayoutManager(this,2)
 
 
     }
---
+++ app/src/main/java/com/ficruty/caocap/LoginSignupActivity.kt
@@ -0,0 +1,14 @@
+package com.ficruty.caocap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginSignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_signup)
+
+
+    }
+}
