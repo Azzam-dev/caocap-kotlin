9ee9e03eb471f2bc8266580f7e4089104f694785
feras272 <ferasdev@gmail.com>
2020-06-13 20:20:38 +0300

    just added coding for the adapter

--- app/src/main/java/com/ficruty/caocap/Controller/MainActivity.kt
+++ app/src/main/java/com/ficruty/caocap/Controller/MainActivity.kt
@@ -13,7 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-//    lateinit var exploreAdapter: ExploreAdapter
+    //lateinit var exploreAdapter:
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
